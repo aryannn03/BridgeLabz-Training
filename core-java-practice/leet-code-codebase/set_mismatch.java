7bf4f3dd1529599cfaafc8ed5ff084507a93728a
aryan <124302386+aryannn03@users.noreply.github.com>
2025-12-16 22:32:36 +0530

    leetcode questions done

---
+++ core-java-practice/leet-code-codebase/set_mismatch.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+        int arr[]= new int[2];
+        int mis=0;
+        int v=1;
+        for(int i =0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                arr[0]=nums[i];
+                break;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=v){
+                mis=v;
+                arr[1]=mis;
+                return arr;
+            }
+            else if(i!= nums.length-1 && nums[i]!=nums[i+1] && nums[i]==v){
+                v++;
+            }
+        }
+        arr[1]=nums[nums.length-1]+1;
+        return arr;
+    }
+}
