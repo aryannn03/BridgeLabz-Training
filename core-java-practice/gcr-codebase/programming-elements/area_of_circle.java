4b484d69ebc9ac6dd0662dccaba78468d7ca0439
aryan <124302386+aryannn03@users.noreply.github.com>
2025-12-17 15:48:56 +0530

    class questions done

---
+++ core-java-practice/gcr-codebase/programming-elements/area_of_circle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class area_of_circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int radius=sc.nextInt();
+        System.out.print(3.14*radius*radius); 
+    }
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/celcius_to_farenheit.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class celcius_to_farenheit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int farenheit=sc.nextInt();
+        System.out.print(farenheit*(9/5)+32);
+    }
+}
