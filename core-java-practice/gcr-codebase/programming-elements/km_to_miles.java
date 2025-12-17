829ed497c47811d1814d0b736d2291e805d97ae9
aryan <124302386+aryannn03@users.noreply.github.com>
2025-12-17 16:37:53 +0530

    class questions done

---
+++ core-java-practice/gcr-codebase/programming-elements/km_to_miles.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class km_to_miles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int km=sc.nextInt();
+        System.out.print(km*0.621371);
+    }
+}
