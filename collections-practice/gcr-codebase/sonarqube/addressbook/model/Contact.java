949f582484a848bc47a72fb06b032fcee61703ac
aryan <124302386+aryannn03@users.noreply.github.com>
2026-02-09 16:07:51 +0530

    [Aryan Malik] Add. sonarqube

---
+++ collections-practice/gcr-codebase/sonarqube/addressbook/model/Contact.java
@@ -0,0 +1,113 @@
+package addressbook.model;
+
+import java.util.Objects;
+
+public class Contact{
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+    private String email;
+
+    public Contact(
+        String firstName,
+        String lastName,
+        String address,
+        String city,
+        String state,
+        String zip,
+        String phone,
+        String email
+    ){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phone=phone;
+        this.email=email;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address=address;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public void setCity(String city){
+        this.city=city;
+    }
+
+    public String getState(){
+        return state;
+    }
+
+    public void setState(String state){
+        this.state=state;
+    }
+
+    public String getZip(){
+        return zip;
+    }
+
+    public void setZip(String zip){
+        this.zip=zip;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+    
+    public boolean equals(Object o){
+        if(this==o)return true;
+        if(o==null||getClass()!=o.getClass())return false;
+        Contact c=(Contact)o;
+        return firstName.equals(c.firstName)&&lastName.equals(c.lastName);
+    }
+
+    public int hashCode(){
+        return Objects.hash(firstName,lastName);
+    }
+
+    public String toString(){
+        return firstName+" "+lastName+" "+address+" "+city+" "+state+" "+zip+" "+phone+" "+email;
+    }
+}
