45bc5c6a5ab1deaea55168e4662e7a3b481a0f1a
aryan <124302386+aryannn03@users.noreply.github.com>
2026-03-31 12:47:10 +0530

    Revert "Merge branch 'main' into jdbc-dbms-practice"
    This reverts commit 1940456f459ff48ff08a5867106d419ee59adaa7, reversing
    changes made to aa9fdfe40bbab7f0c93a1a45aec7fb22076d5dd9.

--- java8-programming-practice/gcr-codebase/lambda-expressions/PatientPrinter.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class PatientPrinter{
-    public static void main(String[] args){
-        List<String> patientIds=List.of("P101","P102","P103","P104");
-        patientIds.forEach(System.out::println);
-    }
-}
--- java8-programming-practice/gcr-codebase/lambda-expressions/UppercaseNames.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class UppercaseNames{
-    public static void main(String[] args){
-        List<String> names=List.of("parth","rahul","neha","simran");
-        names.stream().map(String::toUpperCase).forEach(System.out::println);
-    }
-}
